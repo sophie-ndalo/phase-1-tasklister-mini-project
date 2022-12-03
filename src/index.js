55b298e32f10ed805ff273acf1cbd6a22c342758
sophia <sophiendalo@gmail.com>
2022-12-03 15:04:07 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form");
+ form.addEventListener("submit",handleSubmit);
+
+function handleSubmit(e){
+  console.log(e);
+  e.preventDefault();
+  handleToDo(e.target[0].value);
+form.reset()
+  
+}
 });
+function handleToDo(todo){
+  const li = document.createElement("li");
+  const btn = document.createElement("button")
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = "Delete";
+btn.style.margin = "10px";
+btn.style.fontSize = "15px"
+
+
+
+  li.append(todo);
+  li.appendChild(btn)
+  let ul = document.getElementById("tasks");
+  ul.appendChild(li);
+  // console.log(ul)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+  }
